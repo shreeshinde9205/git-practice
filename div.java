fbccfa63a46fcb439cbcf4ec16be045c877ae418
Shreekrushna <shree.shinde9205@gmail.com>
2026-03-25 13:15:31 +0530

    added Div

---
+++ div.java
@@ -0,0 +1,21 @@
+class Multiplication
+{
+	
+	public static void main(String[] args)
+	{
+		Multiplication M = new Multiplication();
+		System.out.println(Multiplication(1,3);
+		
+	}
+
+	public int static div(int a,int b)
+	{
+		return a/b;
+	}
+
+
+
+
+
+
+}
