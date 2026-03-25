da819edba5bf60cff4911a3700fad63d9e68bc2c
Shreekrushna <shree.shinde9205@gmail.com>
2026-03-25 12:52:39 +0530

    added MUltiply Function

---
+++ mult.java
@@ -0,0 +1,17 @@
+class Multiplication
+{
+	public int static mult(int a,int b)
+	{
+		return a*b;
+	}
+	public static void main(String[] args)
+	{
+		Multiplication M = new Multiplication();
+		System.out.println(Multiplication(1,3);
+		
+	}
+
+
+
+
+}
